f9a12cb4516052a7675262cbe2196348474d8669
Krishnas-world <krishnapallan128@gmail.com>
2024-07-15 12:24:24 +0530

    changed back

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,15 +1,6 @@
-// pages/api/auth/[kindeAuth].js
-import { handleAuth, handleCallback } from "@kinde-oss/kinde-auth-nextjs/server";
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function GET(request, { params }) {
+export async function GET(request, {params}) {
   const endpoint = params.kindeAuth;
-  
-  if (endpoint === 'kinde_callback') {
-    return await handleCallback(request, (req, res, tokenSet) => {
-      // Process the token set and perform any necessary actions (e.g., save user info)
-      res.redirect('/dashboard'); // Redirect to your desired page after successful login
-    });
-  }
-
   return await handleAuth(request, endpoint);
 }
